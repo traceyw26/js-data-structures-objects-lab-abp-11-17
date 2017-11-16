056c0bfaaef7bef7cba8d0ab9f3b139997434fe5
Tracey <tracey.wilhelmsen@gmail.com>
2017-11-16 05:48:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,5 +25,9 @@ function deleteFromDriverByKey(driver, key) {
   key/value pair for the key that was passed in from the driver object.
   This should all not actually mutate the driver passed in.*/
 
+  const newObj = Object.assign({}, driver);
+
+  delete newObj.driver;
+
   return Object.assign({}, driver, delete driver.key);
 }
