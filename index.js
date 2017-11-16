c0431f2098027429c7b32caf759dbe291f1ea385
Tracey <tracey.wilhelmsen@gmail.com>
2017-11-16 04:27:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue() {
-  /*this function should take in a driver object, a key and a value. 
-  The function should not mutate the driver parameter and return a new 
+  /*this function should take in a driver object, a key and a value.
+  The function should not mutate the driver parameter and return a new
   driver that has an updated value for the key passed in.*/
-  const driver = {driver: "Sam"};
+  const driver = {name: "Sam"};
 
   return Object.assign(driver, {address: "11 Broadway"});
 }
